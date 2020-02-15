6fd549af38a0016764cef0ec80891d7ff225b5da
Gerardo <Villalobos>
2020-02-15 08:59:28 -0600

    Model, Service y OrderViewModel

---
+++ XamarinApiAzure/Helpers/Services.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace XamarinApiAzure.Helpers
+{
+    using Newtonsoft.Json;
+    using System.Collections.ObjectModel;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using XamarinApiAzure.Models;
+
+    public class Services
+    {
+        HttpClient client;
+
+        public string WebAPIUrl { get; private set; }
+        public ObservableCollection<Order> Items { get; private set; }
+        public Services()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<ObservableCollection<Order>> RefreshDataAsync()
+        {
+            WebAPIUrl = "https://ena.azurewebsites.net/api/order";
+            var uri = new Uri(WebAPIUrl);
+            try
+            {
+                var response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Items = JsonConvert.DeserializeObject<ObservableCollection<Order>>(content);
+                    return Items;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return null;
+        }
+    }
+}
---
+++ XamarinApiAzure/ViewModels/OrderViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApiAzure.ViewModels
+{
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using XamarinApiAzure.Helpers;
+    using XamarinApiAzure.Models;
+
+    public class OrderViewModel: INotifyPropertyChanged
+    {
+        Services webAPIService;
+        public event PropertyChangedEventHandler PropertyChanged;
+        ObservableCollection<Order> items;
+
+        public ObservableCollection<Order> Items
+        {
+            get { return items; }
+            set
+            {
+                items = value;
+                RaisepropertyChanged("Items");
+            }
+        }
+
+        public OrderViewModel()
+        {
+            webAPIService = new Services();
+            Items = new ObservableCollection<Order>();
+            GetDataFromAPI();
+        }
+
+        async void GetDataFromAPI()
+        {
+            Items = await webAPIService.RefreshDataAsync();
+        }
+
+        void RaisepropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
